7229d9fb7c291dd44c4645c623c10e2c6dd0a5d5
unkown-devil <t.tharunsinghbabu@gmail.com>
2021-04-18 20:18:53 +0530

    product activity

--- app/src/main/java/com/example/livemart/CartActivity.java
+++ app/src/main/java/com/example/livemart/CartActivity.java
@@ -1,14 +1,26 @@
 package com.example.livemart;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class CartActivity extends AppCompatActivity {
+public class CartActivity extends AppCompatActivity
+{
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+
+    private Button NextProcessBtn;
+    private TextView txtTotalAmount, txtMsg1;
+
+    private int overTotalPrice = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
+
     }
-}
+  }
