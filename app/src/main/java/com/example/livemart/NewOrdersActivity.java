e720c2ca0c942b970964200312fdee438e6c2c55
unkown-devil <t.tharunsinghbabu@gmail.com>
2021-04-19 18:30:05 +0530

    order

--- app/src/main/java/com/example/livemart/NewOrdersActivity.java
+++ app/src/main/java/com/example/livemart/NewOrdersActivity.java
@@ -1,14 +1,25 @@
 package com.example.livemart;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
-public class NewOrdersActivity extends AppCompatActivity {
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class NewOrdersActivity extends AppCompatActivity
+{
+    private RecyclerView ordersList;
+    private DatabaseReference ordersRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_orders);
+
+        ordersRef = FirebaseDatabase.getInstance().getReference().child("Orders");
+
     }
 }
